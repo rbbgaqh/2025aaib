ed6f9d9b7026e959a3d7f8132ff8e32e258f4df3
rbbgaqh <13750502@me.mcu.edu.tw>
2025-02-19 11:56:21 +0800

    week01-1

---
+++ week01-1.cpp
@@ -0,0 +1,9 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-S 存檔程 WEEK01-1cpp 要記得加 .cpp
+/// 下周考試考這題, 不用寫藍色的「中文註解」
+#include<stdio.h>///檔名不能寫錯
+/// stido 的意思是 標準的standard input/output
+///有了 stido.h的
+
+
